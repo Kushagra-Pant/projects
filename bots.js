808b39f0d2e9f965b660642293dea6c096ad68ee
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-06-26 11:41:06 -0400

    Update bots.js

--- bots.js
+++ bots.js
@@ -1,7 +1,7 @@
 function expand(element, text){
     buttons = document.getElementsByClassName("expand")
     for(i = 0; i < buttons.length; i++){
-        if (buttons[i] == element){
+        if (buttons[i] === element){
             buttons[i].style.display = 'none'
             document.getElementsByClassName("buttondesc")[i].innerHTML = text
             document.getElementsByClassName("seeless")[i].style.display = 'inline-block'
@@ -12,10 +12,20 @@ function expand(element, text){
 function seeless(element){
     elems = document.getElementsByClassName("seeless")
     for(i = 0; i < elems.length; i++){
-        if (elems[i] == element){
+        if (elems[i] === element){
+            console.log("B")
             elems[i].style.display = 'none'
             document.getElementsByClassName("buttondesc")[i].innerHTML = ""
             document.getElementsByClassName("expand")[i].style.display = 'inline-block'
         }
     }
+}
+
+function setexample(image, header){
+    for(i = 0; i < document.getElementsByClassName("eg").length; i++){
+        document.getElementsByClassName("eg")[i].style.display = "none"
+    }
+    document.getElementById("exampleimage").src=image
+    document.getElementById("exampleheader").innerHTML = header;
+    document.getElementById("exampleheader").style.display = 'block';
 }
